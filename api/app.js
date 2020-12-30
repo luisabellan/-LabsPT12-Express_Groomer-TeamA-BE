84eb393632e73460dc6d571a56d654a1fcbb576b
Luis Abellan <luis.abellan.dev@gmail.com>
2020-12-30 14:45:22 +0100

    fixing cors issue
    'Access-Control-Allow-Origin', '*'

--- api/app.js
+++ api/app.js
@@ -38,6 +38,14 @@ app.use(
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec, swaggerUIOptions)
 );
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 app.use(helmet());
 app.use(express.json());
